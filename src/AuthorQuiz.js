4caa442a26a4c89016dd19b4621ba03da5ba2c5c
samboudjebla <samboudjebla@hotmail.com>
2020-06-10 17:22:41 -0700

    Updated JS files to reflect Project name

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css'
+import { Component } from 'react';
+
+
+class AuthorQuiz extends Component {
+    render() {
+        return (
+            <div>
+                Author Quiz
+            </div>
+        );
+        }
+
+}
+
+
+
+
+
+
+
+
+
+export default AuthorQuiz;
--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
+import AuthorQuiz from './AuthorQuiz';
+
+
+
+
+describe("Author Quiz", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<AuthorQuiz />, div);
+        });
+
+        });
+
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
