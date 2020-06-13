2b773061e35d6bfef0260b2f90763b1a83c0c9cc
samboudjebla <samboudjebla@hotmail.com>
2020-06-13 11:56:09 -0700

    added pictures and the header

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -4,16 +4,66 @@ import './App.css';
 import './bootstrap.min.css'
 import { Component } from 'react';
 
+function Hero() {
+    return (
+        <div className="row">
+            <div className="jumbotron col-10 offset-1">
+                <h1>
+                    Author Quiz
+                </h1>
+                <p>
+                    select the book written by the author shown
+                </p>
+            </div>
+        </div>
+    );
+}
 
-class AuthorQuiz extends Component {
-    render() {
+function Turn(author, books) {
+    return (<div className="row turn" style={{ backgroundColor: "white" }}>
+        <div className="col-4 offset-1">
+            <img src={author.imageUrl} className="authorimage" alt="Author" />
+        </div>
+    </div>);
+}
+
+
+function Book({ title }) {
+    return (<div className="answer">
+        <h4>{title}</h4>
+    </div>
+    );
+}
+
+function Continue() {
+    return <div />;
+}
+
+function Footer() {
+    return (
+        <div id="footer" className="row">
+            <div className="col-12">
+                <p className="text-muted credit">
+                    All images are from
+                        <a href="https://commons.wikimedia.org/wiki/Main_Page"> Wikimedia Commons </a>
+                    and are in the public domain
+                </p>
+            </div>
+        </div>
+    );
+}
+
+
+
+function AuthorQuiz (turnData)  {
         return (
-            <div>
-                Author Quiz
+            <div className="container-fluid">
+                <Hero />
+                <Turn {...turnData} />
+                <Continue />
+                <Footer />
             </div>
         );
-        }
-
 }
 
 
